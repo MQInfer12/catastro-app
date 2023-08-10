d2ac0cc781dcb3cd3e026b0bea5787a910102723
arleking666 <veymar15ganci@gmail.com>
2023-08-10 11:17:05 -0400

    mapview

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,47 @@
+import { MapContainer, TileLayer } from 'react-leaflet'
+import './app.css'
+import 'leaflet/dist/leaflet.css'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+const LAYERS={
+  leaflet:'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  calles:'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+  hibrido:'http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
+  satelite:'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+  terreno:'http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}'
+}
+
+type Layers = keyof typeof LAYERS;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [layer, setLayer]=useState<Layers>("calles");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <MapContainer id='Mapa' center={[-17.39481762066563, -66.1594660433327]} zoom={13} zoomControl={false}>
+        <TileLayer key={layer} url={LAYERS[layer]}
+         subdomains={layer != 'leaflet' ? ['mt0', 'mt1', 'mt2', 'mt3'] : 'abc'}
+        />
+
+      </MapContainer>
+      <div className='button-view'>
+        <button onClick={()=> setLayer('leaflet')}>
+          leaflet
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={()=> setLayer('calles')}>
+          Calles
+        </button>
+        <button onClick={()=> setLayer('hibrido')}>
+          Hibrido
+        </button>
+        <button onClick={()=> setLayer('satelite')}>
+          Satelite
+        </button>
+        <button onClick={()=> setLayer('terreno')}>
+          terreno
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
